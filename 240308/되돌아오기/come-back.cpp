ab4e6cf2b07cec559897d05d9180185318b358ce
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-03-08 11:49:32 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240308/되돌아오기/come-back.cpp
+++ 240308/되돌아오기/come-back.cpp
@@ -14,55 +14,57 @@ int main() {
         scanf(" %c %d", &dir[i], &cost[i]);
         // printf("%c %d\n", dir[i], cost[i]);
     }
-    for(int i = 0; i<n; ++i){
-        switch(dir[i]){
+    int iter = 0;
+    for(int iter = 0; iter<n; ++iter){
+        switch(dir[iter]){
         case 'W':
-            if(dx > 0 && dx <= cost[i] && dy == 0){
+            if(dx > 0 && dx <= cost[iter] && dy == 0){
                 sum += dx;
                 dx = 0;
             }
             else{
-                dx -= cost[i];
-                sum += cost[i];
+                dx -= cost[iter];
+                sum += cost[iter];
             }
             break;
         case 'S':
-            if(dy > 0 && dy <= cost[i] && dx == 0){
+            if(dy > 0 && dy <= cost[iter] && dx == 0){
                 sum += dy;
                 dy = 0;
             }
             else{
-                dy -= cost[i];
-                sum += cost[i];
+                dy -= cost[iter];
+                sum += cost[iter];
             }
             break;
         case 'N':
-            if(dy < 0 && (dy + cost[i]) >= 0 && dx == 0){
+            if(dy < 0 && (dy + cost[iter]) >= 0 && dx == 0){
                 sum += abs(dy);
                 dy = 0;
             }
             else{
-                dy += cost[i];
-                sum += cost[i];
+                dy += cost[iter];
+                sum += cost[iter];
             }
             break;
         case 'E':
-            if(dx < 0 && (dx + cost[i]) >= 0 && dy == 0){
+            if(dx < 0 && (dx + cost[iter]) >= 0 && dy == 0){
                 sum += abs(dx);
                 dx = 0;
             }
             else{
-                dx += cost[i];
-                sum += cost[i];
+                dx += cost[iter];
+                sum += cost[iter];
             }
             break;
         }
-        // printf("%d %d\n", i, sum);
+        // printf("%d %d\n", iter, sum);
         // printf("%d %d\n\n", dx, dy);
         if(dx == 0 && dy == 0) break;
     }
 
-    printf("%d", sum);
+    if(iter == n) printf("%d", -1);
+    else printf("%d", sum);
 
     return 0;
 }
