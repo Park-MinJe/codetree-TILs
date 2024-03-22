b84d85178383f58247585c24c6eaa0d67085940f
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-03-22 17:27:30 +0900

    Title: 동전 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240322/동전 더하기/add-coins.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, k, cnt = 0;
+vector<int> coins;
+
+int main() {
+    scanf("%d %d", &n, &k);
+    for(int i = 0; i < n; ++i){
+        int tmpC;
+        scanf(" %d", &tmpC);
+        coins.push_back(tmpC);
+    }
+
+    for(int i = n-1; i >= 0; --i){
+        if(k == 0){
+            printf("%d", cnt);
+            break;
+        }
+        int tmpCnt = k/coins[i];
+        cnt += tmpCnt;
+        k -= tmpCnt*coins[i];
+    }
+    
+    return 0;
+}
