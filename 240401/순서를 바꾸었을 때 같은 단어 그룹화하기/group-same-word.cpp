67aeb364cc22598933527bd4c0718510de304b82
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-04-01 14:49:46 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 73ms, Memory: 2MB, Status: Passed - CodeTree

--- 240401/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
+++ 240401/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <unordered_map>
 #include <queue>
+#include <algorithm>
 
 #define MAX_N 1001
 
@@ -9,55 +10,69 @@ using namespace std;
 
 int n, maxGroupSize = 0;
 string arr[MAX_N];
-unordered_map<char, int> alphabetCntMap[MAX_N];
-bool isVisitedWord[MAX_N];
 
-bool isSameMap(unordered_map<char, int> m, unordered_map<char, int> trgtM){
-    int tmpAlphabetNum = m.size();
+// unordered_map<char, int> alphabetCntMap[MAX_N];
+// bool isVisitedWord[MAX_N];
 
-    if(tmpAlphabetNum != trgtM.size())
-        return false;
+unordered_map<string, int> wordCntMap;
+
+// bool isSameMap(unordered_map<char, int> m, unordered_map<char, int> trgtM){
+//     int tmpAlphabetNum = m.size();
+
+//     if(tmpAlphabetNum != trgtM.size())
+//         return false;
     
-    unordered_map<char, int>::iterator itM;
-    for(itM = m.begin(); itM != m.end(); ++itM){
-        if(trgtM.find(itM->first) == trgtM.end()){    // 없으면
-            return false;
-        }
-        else{
-            if(itM->second != trgtM[itM->first]){
-                return false;
-            }
-        }
-    }
-    return true;
-}
+//     unordered_map<char, int>::iterator itM;
+//     for(itM = m.begin(); itM != m.end(); ++itM){
+//         if(trgtM.find(itM->first) == trgtM.end()){    // 없으면
+//             return false;
+//         }
+//         else{
+//             if(itM->second != trgtM[itM->first]){
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
 
-void search(){
-    for(int i = 0; i < n; ++i){
-        if(!isVisitedWord[i]){
-            int tmpGroupSize = 1;
+// void search(){
+//     for(int i = 0; i < n; ++i){
+//         if(!isVisitedWord[i]){
+//             int tmpGroupSize = 1;
 
-            for(int j = i+1; j < n; ++j){
-                if(!isVisitedWord[i] && isSameMap(alphabetCntMap[i], alphabetCntMap[j])){
-                    ++tmpGroupSize;
-                }
-            }
-            if(maxGroupSize < tmpGroupSize)
-                maxGroupSize = tmpGroupSize;
-        }
-    }
+//             for(int j = i+1; j < n; ++j){
+//                 if(!isVisitedWord[i] && isSameMap(alphabetCntMap[i], alphabetCntMap[j])){
+//                     ++tmpGroupSize;
+//                 }
+//             }
+//             if(maxGroupSize < tmpGroupSize)
+//                 maxGroupSize = tmpGroupSize;
+//         }
+//     }
+// }
+
+bool compare(pair<string, int>& p1, pair<string, int>& p2){
+    return p1.second > p2.second;
 }
 
 int main() {
     cin>>n;
     for(int i = 0; i < n; ++i){
         cin>>arr[i];
-        isVisitedWord[i] = false;
-        for(int j = 0; j < arr[i].length(); ++j){
-            ++alphabetCntMap[i][arr[i][j]];
-        }
+        sort(arr[i].begin(), arr[i].end());
+        // isVisitedWord[i] = false;
+        // for(int j = 0; j < arr[i].length(); ++j){
+        //     ++alphabetCntMap[i][arr[i][j]];
+        // }
+        ++wordCntMap[arr[i]];
     }
-    search();
-    cout<<maxGroupSize;
+    //search();
+    //cout<<maxGroupSize;
+
+    vector<pair<string, int>> vecWordCntMap(wordCntMap.begin(), wordCntMap.end());
+    sort(vecWordCntMap.begin(), vecWordCntMap.end(), compare);
+    cout<<vecWordCntMap[0].second;
+
     return 0;
 }
