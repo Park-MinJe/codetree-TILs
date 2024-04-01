1636cda2d9b9f091ed07bfd58f53b6ce215cd229
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-04-01 10:24:46 +0900

    Title: 두 수의 합, Time: 171ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 240401/두 수의 합/sum-of-two-num.cpp
+++ 240401/두 수의 합/sum-of-two-num.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-int n, k;
-long long cnt;
+int n;
+long long k, cnt;
 unordered_map<long long, vector<int>> map;
 unordered_map<long long, bool> mapIsUsed;
 
@@ -22,7 +22,7 @@ int main() {
     for(it = map.begin(); it != map.end(); ++it){
         if(!mapIsUsed[it->first]){
             mapIsUsed[it->first] = true;
-            int minus = k - it->first;
+            long long minus = k - it->first;
             if(minus != it->first){
                 if(!mapIsUsed[minus]){
                     mapIsUsed[minus] = true;
