5d04d771b4e9ca9e6362d80bf975a7de956ea997
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-04-01 10:26:47 +0900

    Title: 두 수의 합, Time: 89ms, Memory: 6MB, Status: Passed - CodeTree

--- 240401/두 수의 합/sum-of-two-num.cpp
+++ 240401/두 수의 합/sum-of-two-num.cpp
@@ -1,44 +1,78 @@
+// #include <iostream>
+// #include <unordered_map>
+// #include <vector>
+
+// using namespace std;
+
+// int n, k;
+// long long cnt;
+// unordered_map<long long, vector<int>> map;
+// unordered_map<long long, bool> mapIsUsed;
+
+// int main() {
+//     cin>>n>>k;
+//     for(int i = 0; i < n; ++i){
+//         long long tmp;
+//         cin>>tmp;
+//         map[tmp].push_back(i);
+//         mapIsUsed[tmp] = false;
+//     }
+    
+//     unordered_map<long long, vector<int>>::iterator it;
+//     for(it = map.begin(); it != map.end(); ++it){
+//         if(!mapIsUsed[it->first]){
+//             mapIsUsed[it->first] = true;
+//             int minus = k - it->first;
+//             if(minus != it->first){
+//                 if(!mapIsUsed[minus]){
+//                     mapIsUsed[minus] = true;
+//                     cnt += it->second.size() * map[minus].size();
+//                 }
+//                 //cout<<minus<<" "<<cnt<<"\n";
+//             }
+//             else{
+//                 int tmpSize = (it->second).size();
+//                 cnt += tmpSize * (tmpSize-1) / 2;
+//             }
+            
+//         }
+//     }
+
+//     cout<<cnt;
+    
+//     return 0;
+// }
+
 #include <iostream>
 #include <unordered_map>
-#include <vector>
 
 using namespace std;
 
-int n;
-long long k, cnt;
-unordered_map<long long, vector<int>> map;
-unordered_map<long long, bool> mapIsUsed;
+#define MAX_N 100000
+
+// 변수 선언
+int n, k;
+int arr[MAX_N];
+unordered_map<long long, int> count;
 
 int main() {
-    cin>>n>>k;
-    for(int i = 0; i < n; ++i){
-        long long tmp;
-        cin>>tmp;
-        map[tmp].push_back(i);
-        mapIsUsed[tmp] = false;
-    }
-    
-    unordered_map<long long, vector<int>>::iterator it;
-    for(it = map.begin(); it != map.end(); ++it){
-        if(!mapIsUsed[it->first]){
-            mapIsUsed[it->first] = true;
-            long long minus = k - it->first;
-            if(minus != it->first){
-                if(!mapIsUsed[minus]){
-                    mapIsUsed[minus] = true;
-                    cnt += it->second.size() * map[minus].size();
-                }
-                //cout<<minus<<" "<<cnt<<"\n";
-            }
-            else{
-                int tmpSize = (it->second).size();
-                cnt += tmpSize * (tmpSize-1) / 2;
-            }
-            
-        }
+    // 입력:
+    cin >> n >> k;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int ans = 0;
+
+    // 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+    for(int i = 0; i < n; i++) {
+        long long diff = (long long)k - arr[i];
+        // 가능한 모든 쌍의 수를 세어줍니다.
+        ans += count[diff];
+
+        // 현재 숫자의 개수를 하나 증가시켜줍니다.
+        count[arr[i]]++;
     }
 
-    cout<<cnt;
-    
+    cout << ans;
     return 0;
 }
