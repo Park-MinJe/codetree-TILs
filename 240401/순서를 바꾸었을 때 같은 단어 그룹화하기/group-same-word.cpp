2774db971a5491d4da9abf0dc8bfc92ce551f5d5
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-04-01 14:39:53 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 1074ms, Memory: 2MB, Status: Time Limit Exceed - CodeTree

---
+++ 240401/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <queue>
+
+#define MAX_N 1001
+
+using namespace std;
+
+int n, maxGroupSize = 0;
+string arr[MAX_N];
+unordered_map<char, int> alphabetCntMap[MAX_N];
+bool isVisitedWord[MAX_N];
+
+bool isSameMap(unordered_map<char, int> m, unordered_map<char, int> trgtM){
+    int tmpAlphabetNum = m.size();
+
+    if(tmpAlphabetNum != trgtM.size())
+        return false;
+    
+    unordered_map<char, int>::iterator itM;
+    for(itM = m.begin(); itM != m.end(); ++itM){
+        if(trgtM.find(itM->first) == trgtM.end()){    // 없으면
+            return false;
+        }
+        else{
+            if(itM->second != trgtM[itM->first]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void search(){
+    for(int i = 0; i < n; ++i){
+        if(!isVisitedWord[i]){
+            int tmpGroupSize = 1;
+
+            for(int j = i+1; j < n; ++j){
+                if(!isVisitedWord[i] && isSameMap(alphabetCntMap[i], alphabetCntMap[j])){
+                    ++tmpGroupSize;
+                }
+            }
+            if(maxGroupSize < tmpGroupSize)
+                maxGroupSize = tmpGroupSize;
+        }
+    }
+}
+
+int main() {
+    cin>>n;
+    for(int i = 0; i < n; ++i){
+        cin>>arr[i];
+        isVisitedWord[i] = false;
+        for(int j = 0; j < arr[i].length(); ++j){
+            ++alphabetCntMap[i][arr[i][j]];
+        }
+    }
+    search();
+    cout<<maxGroupSize;
+    return 0;
+}
