e40f6389a19cc34ecfa12c4900a00bd95ec8f617
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-04-03 11:32:46 +0900

    Title: 괄호 쌍 만들어주기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240403/괄호 쌍 만들어주기/pair-parentheses.cpp
+++ 240403/괄호 쌍 만들어주기/pair-parentheses.cpp
@@ -7,7 +7,9 @@ using namespace std;
 
 string str;
 int open = 0,
-    close = 0;
+    close = 0,
+    R[MAX_LEN],
+    ans = 0;
 
 int getnC2(int n){
     return n * (n-1) / 2;
@@ -16,14 +18,19 @@ int getnC2(int n){
 int main() {
     cin>>str;
     int strLen = str.size();
-    for(int i = 0; i < strLen-1; ++i){
-        if(str[i] == '(' && str[i+1] == '('){
-            ++open;
+    for(int i = strLen-3; i >= 1; --i){
+        if(str[i+1] == ')' && str[i+2] == ')'){
+            R[i] = R[i+1] + 1;
         }
-        if(str[i] == ')' && str[i+1] == ')'){
-            ++close;
+        else{
+            R[i] = R[i+1];
         }
     }
-    cout<<open * close;
+    for(int i = 1; i < strLen-2; ++i){
+        if(str[i-1] == '(' && str[i] == '('){
+            ans += R[i];
+        }
+    }
+    cout<<ans;
     return 0;
 }
