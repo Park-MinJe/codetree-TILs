9c7327b8a0a4dd9c77b3853e42ec79b916cfe871
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-04-03 14:14:08 +0900

    Title: 이상한 폭탄, Time: 90ms, Memory: 2MB, Status: Passed - CodeTree

--- 240403/이상한 폭탄/strange-bomb.cpp
+++ 240403/이상한 폭탄/strange-bomb.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 #define MAX_N 200001
 
@@ -6,7 +7,7 @@ using namespace std;
 
 int n,  // 폭탄 수
     k,  // 폭발하는 거리
-    bombs[MAX_N],
+    bombs[MAX_N], R[MAX_N],
     ans = -1;    // 폭발할 폭탄의 가장 큰 번호
 
 void init(){
@@ -17,12 +18,21 @@ void init(){
 }
 
 void solve(){
-    for(int i = 0; i < n-k; ++i){
-        for(int j = i+1 ; j <= i+k; ++j){
-            if(bombs[j] == bombs[i]){
-                if(ans < bombs[i]) ans = bombs[i];
-                break;
-            }
+    unordered_map<int, int> latest_index;
+    for(int i = n-1; i >= 0; --i){
+        if(latest_index.find(bombs[i]) == latest_index.end()){
+            R[i] = -1;
+        }
+        else{
+            R[i] = latest_index[bombs[i]];
+        }
+
+        latest_index[bombs[i]] = i;
+    }
+
+    for(int i = 0; i < n; ++i){
+        if(R[i] != -1 && R[i] - i <= k){
+            ans = max(ans, bombs[i]);
         }
     }
 }
